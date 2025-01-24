aeeb558f5ad4dccb33e61c2bba7042b560f71c65
JeanKim7 <jean.g.km@gmail.com>
2025-01-24 17:43:33 -0500

    add navigation upon login/logout, add sign in method

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 
@@ -36,7 +36,7 @@ const signup = async (username, email, password) => {
       })
     } catch (error) {
       console.error(error);
-      toast.error(error.code)
+      toast.error(error.code.split('/')[1].split('-').join(" "))
     }
     }
 
@@ -46,8 +46,18 @@ const login = async (email,password) => {
 
     } catch (error) {
       console.error(error)
-      toast.error(error.code)
+      toast.error(error.code.split('/')[1].split('-').join(" "))
     }
 }
+
+const logout = async () => {
+  try {
+      await signOut(auth)
+  } catch (error) {
+        console.error(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "))
+  } 
+  
+}
     
-    export {signup, login}
+    export {signup, login, logout, auth, db}
